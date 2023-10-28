c094b1b07ec9360f67acf0f7b77973d3427396c0
shashank <shashankchandanagiri@gmail.com>
2023-10-28 17:08:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose'); // Import Mongoose
+mongoose.set('strictQuery', true); // Set strictQuery to false
+
+  mongoose.connect('mongodb://127.0.0.1/shashank', {
+    useNewUrlParser: true,
+  });
+
+
+// Define a Mongoose schema for your form data
+const formDataSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  email_sub: String,
+  message: String
+});
+
+// Create a Mongoose model based on the schema
+const FormData = mongoose.model('FormData', formDataSchema);
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json())
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
+
+app.post('/submitform', async (req, res) => {
+  try {
+    // Create a new document using the FormData model
+    const formData = new FormData({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      email_sub: req.body.email_sub,
+      message: req.body.message
+    });
+
+    // Save the document to the MongoDB database
+    await formData.save();
+
+    res.status(200).send('Form data submitted successfully.');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred while processing the form data.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ script.js
@@ -0,0 +1,71 @@
+// Toggle icon navbar************************
+let menuIcon =document.querySelector('#menu-icon');
+let Navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    Navbar.classList.toggle('active');
+};
+// scroll activiation **************************
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+// **********************************************************************************************************************************
+const toggleSwitch = document.getElementById("toggle");
+const body = document.body;
+
+toggleSwitch.addEventListener("change", () => {
+    if (toggleSwitch.checked) {
+        body.classList.add("dark-mode");
+    } else {
+        body.classList.remove("dark-mode");
+    }
+});
+
+// **********************************************************************************************************************************
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top =window.scrollY;
+        let offset =sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id =sec.getAttribute('id');
+        
+        if(top>= offset && top < offset + height) {
+            navlinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        };
+    });
+    // sticky navbar********************************************
+    let header =document.querySelector('header');
+    
+    header.classList.toggle('sticky',window.scrollY>100);
+    
+    // remove toggle icon and navbar when click navbar link***************************
+    menuIcon.classList.remove('bx-x');
+    Navbar.classList.remove('active');
+};
+
+    // scroll revrsal********************************************
+    ScrollReveal({ 
+        // reset: true,
+        distance:'80px',
+        duration:2000,
+        delay:200
+
+     });
+
+     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+     ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+     
+     ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' });
+     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+    //  typed js ************************************
+    const typed =new Typed('.multiple-text',{
+        strings: ['Frontend Developer', 'Backend Developer', 'Full Stack Developer'],
+        typespeed:500,
+        backspeed:500,
+        backdealy:8000,
+        loop:true
+    });
